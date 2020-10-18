5ed265fd6274ba5b6f5c7c4b1f4b5ed6c5352d5e
Alvaro Ariño Cabau <alvaro.arino.cabau@gmail.com>
2020-10-18 20:53:11 +0200

    Classe usuari creada. Modificat el .gitignore per admetre la carpeta src

---
+++ src/Usuari.java
@@ -0,0 +1,12 @@
+public class Usuari {
+    String _nom;
+    // Imatge _fotoPerfil;
+    String _usuari;
+    String _contrasenya;
+
+    Usuari(String nom, String usuari, String contrasenya) {
+        this._nom = nom;
+        this._usuari = usuari;
+        this._contrasenya = contrasenya;
+    }
+}
