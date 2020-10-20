ac7f0a08183381ee1231cbbb6085f46f7513fbc0
Victor Manuel Delgado Padilla <victor.manuel.delgado@estudiantat.upc.edu>
2020-10-20 09:29:56 +0200

    Creació dels paquets

--- src/Usuari.java
+++ src/Usuari.java
@@ -1,9 +1,28 @@
+/**
+ * The type Usuari.
+ */
 public class Usuari {
+    /**
+     * The Nom.
+     */
     String _nom;
-    // Imatge _fotoPerfil;
+    /**
+     * The Usuari.
+     */
+// Imatge _fotoPerfil;
     String _usuari;
+    /**
+     * The Contrasenya.
+     */
     String _contrasenya;
 
+    /**
+     * Instantiates a new Usuari.
+     *
+     * @param nom         the nom
+     * @param usuari      the usuari
+     * @param contrasenya the contrasenya
+     */
     Usuari(String nom, String usuari, String contrasenya) {
         this._nom = nom;
         this._usuari = usuari;
