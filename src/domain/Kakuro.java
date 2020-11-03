0af9c7a91e4606f3e1d39e03a63ebeae606049b6
Victor Manuel Delgado Padilla <victor.manuel.delgado@estudiantat.upc.edu>
2020-11-03 10:02:36 +0100

    Generació de Kakuros en procés :)

--- src/domain/Kakuro.java
+++ src/domain/Kakuro.java
@@ -20,8 +20,8 @@ public class Kakuro {
     public Kakuro() {
 
         aleat = new Random();
-       int n = aleat.nextInt(4)+3;
-       int m = aleat.nextInt(4)+3;
+       int n = aleat.nextInt(4)+4;
+       int m = aleat.nextInt(4)+4;
 
         board = new Tauler(n,m);
     }
@@ -30,14 +30,20 @@ public class Kakuro {
 
     public void generar() {
 
+        int minimo = Math.min(board.getDimn(), board.getDimm());
 
-        int ca_negras = ((aleat.nextInt(board.getDimn()* board.getDimm())) / 2) ;
-
-
-
+        int ca_negras = ((board.getDimn()* board.getDimm()) / 2) - minimo ;
 
 
+        board.pintar_negras(ca_negras);
 
+        board.buscar_problemas();
+
+        board.rellenar_blancas();
+
+        board.hacer_sumas();
+
+        board.borrar_blancas();
 
 
 
--- src/domain/Tauler.java
+++ src/domain/Tauler.java
@@ -6,7 +6,7 @@ public class Tauler {
 
     private Cella CjtCelles [][] ; // Conjunt de cel·les
     private int dimn, dimm;
-    
+
     public Tauler(int n, int m) {
 
         dimn = n;
@@ -28,12 +28,30 @@ public class Tauler {
         return dimn;
     }
 
+    public void  pintar_negras(int cantidad) {
 
+        //DE FORMA ALEATORIA -> MITAD aleatoria y espejo
+    }
 
     public void pintar_negra(int x, int y, int  sum_d, int sum_i){
 
     }
     public void pintar_blanca(int x, int y){}
 
+    public void buscar_problemas() {
 
+        //ESTA MÉTODO HACE UNA BÚSQUEDA PARA MIRAR SI HAY +9 CASILLAS JUNTAS y busca otros problemas que no cumplan las reglas KAKURO
+    }
+
+    public void rellenar_blancas() {
+
+    }
+
+    public void hacer_sumas(){
+
+    }
+
+    public void borrar_blancas(){
+
+    }
 }
