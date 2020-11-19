808dcf1f1721811228c19fa8ad030d93f98c3e72
Victor Manuel Delgado Padilla <victor.manuel.delgado@estudiantat.upc.edu>
2020-11-19 10:15:17 +0100

    DRIVER KAKURO :)

--- FONTS/domain/kakuro/KakuroDriver.java
+++ FONTS/domain/kakuro/KakuroDriver.java
@@ -1,28 +1,84 @@
 package domain.kakuro;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class KakuroDriver {
-    public void testGenerar() {
-
-    }
-
     public static void main(String[] args) {
         System.out.println("Driver de la classe Kakuro");
-        Scanner entrada = new  Scanner(System.in);
-        int n, m, negres, blanques;
-        System.out.println("Introdueix una dimensió, escriu primer el número de files n");
-        n = Integer.parseInt(entrada.nextLine());
-        System.out.println("Ara el número de columnes m");
-        m = Integer.parseInt(entrada.nextLine());
-        System.out.println("Indica el nombre de caselles negres que desitges");
-        negres = Integer.parseInt(entrada.nextLine());
-        System.out.println("Indica el nombre de caselles blanques que desitges");
-        blanques = Integer.parseInt(entrada.nextLine());
-
-        Kakuro kk = new Kakuro(n, m);
-        kk.generar_usuario(n,m,negres,blanques);
+        Kakuro kakuro = new Kakuro();
 
 
+
+        try {
+            BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("Introdueix una opció i els paràmetres corresponents:");
+            System.out.println("0. Sortir del driver");
+            System.out.println("1. Generar Kakuro amb paràmetres d'usuari");
+            System.out.println("2. Generar Kakuro aleatori");
+
+
+
+            boolean fi = false;
+            while (!fi){
+
+                String linea, opcion;
+                String [] entrada;
+                linea = b.readLine();
+                entrada = linea.split(" ");
+                opcion = entrada[0];
+
+                try {
+
+                    System.out.println("Has seleccionat l'opció " + opcion);
+                    switch (opcion){
+                        case "0":
+                            fi = true;
+                            break;
+                        case "1":
+                            Scanner intro = new Scanner(System.in);
+                            int n, m, negres, blanques;
+                            System.out.println("Introdueix una dimensió, escriu primer el número de files n");
+                            n = Integer.parseInt(intro.nextLine());
+                            System.out.println("Ara el número de columnes m");
+                            m = Integer.parseInt(intro.nextLine());
+                            System.out.println("Indica el nombre de caselles negres que desitges");
+                            negres = Integer.parseInt(intro.nextLine());
+                            System.out.println("Indica el nombre de caselles blanques que desitges");
+                            blanques = Integer.parseInt(intro.nextLine());
+
+                            Kakuro kk = new Kakuro();
+                            kk.generar_usuario(n, m, negres, blanques);
+                            break;
+
+                        case "2":
+                            kakuro.generar();
+                            System.out.println("S'ha creat un Kakuro de forma al·leatòria");
+                            break;
+
+                        default:
+                            System.out.println(opcion + " no és una opció");
+                    }
+                }
+                catch (Exception e){
+                    System.out.println(e.getMessage());
+                }
+
+            }
+            System.out.println("Fi del driver");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
+
+
+
+
+
+
+
+
