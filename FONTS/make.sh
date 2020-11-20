445b8a70a62d1396718dc912b799611ec5510e2b
Alvaro Ariño Cabau <alvaro.arino@est.fib.upc.edu>
2020-11-20 23:25:21 +0100

    Makefile

---
+++ FONTS/make.sh
@@ -0,0 +1,26 @@
+javac  -d ../EXE/DriverCella domain/cella/Cella.java;
+javac  -d ../EXE/DriverCella domain/cella/CellaNegra.java;
+javac  -d ../EXE/DriverCella domain/cella/CellaBlanca.java;
+javac  -d ../EXE/DriverCella domain/cella/DriverCella.java;
+
+javac  -d ../EXE/KakuroDriver domain/kakuro/Kakuro.java;
+javac  -d ../EXE/KakuroDriver domain/kakuro/Tauler.java;
+javac  -d ../EXE/KakuroDriver domain/kakuro/KakuroDriver.java;
+javac  -d ../EXE/KakuroDriver domain/kakuro/DriverTauler.java;
+
+javac  -d ../EXE/DriverUsuaris domain/usuari/Perfil.java;
+javac  -d ../EXE/DriverUsuaris  domain/usuari/Usuari.java;
+javac  -d ../EXE/DriverUsuaris  domain/usuari/DriverUsuari.java;
+
+javac  -d ../EXE/DriverPerfil  domain/usuari/Perfil.java;
+javac  -d ../EXE/DriverPerfil  domain/usuari/Usuari.java;
+javac  -d ../EXE/DriverPerfil  domain/usuari/DriverPerfil.java;
+
+javac  -d ../EXE/DriverCtrlDataUsuaris -cp lib/gson.jar dades/CtrlDataUsuaris.java;
+
+javac  -d ../EXE/DriverCtrlDataKakuro domain/cella/Cella.java;
+javac  -d ../EXE/DriverCtrlDataKakuro domain/cella/CellaNegra.java;
+javac  -d ../EXE/DriverCtrlDataKakuro domain/cella/CellaBlanca.java;
+javac  -d ../EXE/DriverCtrlDataKakuro dades/CtrlDataKakuro.java
+javac  -d ../EXE/DriverCtrlDataKakuro dades/DriverCtrlDataKakuro.java;
+
