734aa7bc89181112ed475f1ddd8eb4c2459372a6
Alvaro Ariño Cabau <alvaro.arino@est.fib.upc.edu>
2020-11-20 11:19:10 +0100

    CellaTest

--- FONTS/test/CellaTest.java
+++ FONTS/test/CellaTest.java
@@ -1,43 +1,98 @@
+import domain.cella.Cella;
+import domain.cella.CellaBlanca;
+import domain.cella.CellaNegra;
 import domain.cella.ColorCella;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class CellaTest {
+
+    Cella cella = new Cella();
 
     @Test
     public void Cella() {
+        cella = new CellaBlanca();
+        assertTrue(cella.color() == ColorCella.Blanca);
 
-    }
-
-    public void intro_valor_blanca(int z){
-
-    }
-
-    public int getValorBlancaTest() {
-        return 0;
-    }
-
-
-    public void setValorFilaTest(int val) {}
-
-
-    public void setValorColumnaTest(int val) {}
-
-
-    public void acumular_valorDerechaTest(int s) {}
-
-    public void acumular_valorIzquierdaTest(int s) {}
-
-
-    public ColorCella colorTest() {
-        return ColorCella.None;
-    }
-
-    public int getValorDretTest() {
-        return 0;
+        cella = new CellaNegra();
+        assertTrue(cella.color() == ColorCella.Negra);
     }
 
     @Test
-    public int getValorEsquerreTest() {
-        return 0;
+    public void introValorBlancaTest(){
+        cella = new CellaBlanca();
+        cella.intro_valor_blanca(15);
+        assertTrue(cella.getValor_blanca() == 15);
+    }
+
+    @Test
+    public void getValorBlancaTest() {
+        cella = new CellaBlanca();
+        assertTrue(cella.getValor_blanca() == -1);
+
+        cella.intro_valor_blanca(2);
+        assertTrue(cella.getValor_blanca() == 2);
+    }
+
+    @Test
+    public void setValorFilaTest() {
+        cella = new CellaNegra();
+        cella.setValorFila(15);
+        assertTrue(cella.getValorDret() == 15);
+    }
+
+    @Test
+    public void setValorColumnaTest() {
+        cella = new CellaNegra();
+        cella.setValorColumna(15);
+        assertTrue(cella.getValorEsquerre() == 15);
+    }
+
+    @Test
+    public void acumularValorDerechaTest() {
+        cella = new CellaNegra();
+        assertTrue(cella.getValorDret() == -1);
+        cella.acumular_valor_derecha(15);
+        assertTrue(cella.getValorDret() == 15);
+        cella.acumular_valor_derecha(20);
+        assertTrue(cella.getValorDret() == 35);
+    }
+
+    @Test
+    public void acumularValorIzquierdaTest() {
+        cella = new CellaNegra();
+        assertTrue(cella.getValorEsquerre() == -1);
+        cella.acumular_valor_izquierda(100);
+        assertTrue(cella.getValorEsquerre() == 100);
+        cella.acumular_valor_izquierda(20);
+        assertTrue(cella.getValorEsquerre() == 120);
+    }
+
+    @Test
+    public void colorTest() {
+        cella = new CellaBlanca();
+        assertTrue(cella.color() == ColorCella.Blanca);
+
+        cella = new CellaNegra();
+        assertTrue(cella.color() == ColorCella.Negra);
+    }
+
+    @Test
+    public void getValorDretTest() {
+        cella = new CellaNegra();
+        assertTrue(cella.getValorDret() == -1);
+        cella = new CellaNegra(2,20);
+        assertTrue(cella.getValorDret() == 2);
+        assertTrue(cella.getValorDret() != 20);
+    }
+
+    @Test
+    public void getValorEsquerreTest() {
+        cella = new CellaNegra();
+        assertTrue(cella.getValorEsquerre() == -1);
+        cella = new CellaNegra(2,20);
+        assertTrue(cella.getValorEsquerre() != 2);
+        assertTrue(cella.getValorEsquerre() == 20);
     }
 }
