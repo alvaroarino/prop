78c44c53d53a22ee39e8e514e4b9978ca0b5a727
Victor Manuel Delgado Padilla <victor.manuel.delgado@estudiantat.upc.edu>
2020-12-08 11:01:08 +0100

    Vista Aventura

---
+++ FONTS/presentacion/VistaAventura.java
@@ -0,0 +1,20 @@
+package presentacion;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VistaAventura implements Initializable {
+
+    @FXML
+    Label timeLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        timeLabel.setText("Hola");
+
+    }
+}
