413fe559d02e80d458b39ca03e749aa3b301d0ac
Victor Manuel Delgado Padilla <victor.manuel.delgado@estudiantat.upc.edu>
2020-12-13 22:02:29 +0100

    Controlador Dades actualització (incompleta)

--- FONTS/dades/CtrlDades.java
+++ FONTS/dades/CtrlDades.java
@@ -4,10 +4,38 @@ public class CtrlDades {
      private CtrlDades ctrlDades;
 
 
+
     public CtrlDades() {
         ctrlDades = new CtrlDades();
 
     }
+
+    public void guardar_kakuro() {
+
+
+    }
+
+    public void guardar_perfil() {
+
+
+    }
+
+    public void leer_perfil() {
+
+
+    }
+
+    public void leer_kakuro() {
+
+
+    }
+
+    public void leer_estadisticas() {
+
+        
+    }
+
+
 
 
 
