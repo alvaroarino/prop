21be21567389a2aaacf701cae926e04169ef1625
Victor Manuel Delgado Padilla <victor.manuel.delgado@estudiantat.upc.edu>
2020-12-14 08:58:33 +0100

    Controlador Dades actualització 14/12/2020

--- FONTS/dades/CtrlDades.java
+++ FONTS/dades/CtrlDades.java
@@ -1,39 +1,56 @@
 package dades;
 
-public class CtrlDades {
-     private CtrlDades ctrlDades;
+import domain.kakuro.Kakuro;
+import domain.usuari.Perfil;
 
+public class CtrlDades {
+     private CtrlDataPerfil  perfil = new CtrlDataPerfil();
+    private CtrlDataAventura  aventura = new CtrlDataAventura();
+    private CtrlDataPartida  partida = new CtrlDataPartida();
+    private CtrlDataKakuro  kakuro = new CtrlDataKakuro();
+    private CtrlDataUsuaris usuaris = new CtrlDataUsuaris();
+    private CtrlDataRanking ranking = new CtrlDataRanking();
 
 
     public CtrlDades() {
-        ctrlDades = new CtrlDades();
-
-    }
-
-    public void guardar_kakuro() {
 
 
     }
 
-    public void guardar_perfil() {
-
-
-    }
-
-    public void leer_perfil() {
-
+    public void guardar_kakuro(int id, Kakuro k) {
+    kakuro.guardarKakuro(id,k);
 
     }
 
-    public void leer_kakuro() {
+    public void guardar_perfil(Perfil p, String nom) {
+
+        perfil.guardarPerfil(p,nom);
+
+    }
+
+    public void leer_perfil(String nom) {
+
+        perfil.getPerfil(nom);
+
+    }
+
+    public void leer_kakuro(String archivo) {
+        kakuro.getData(archivo);
 
 
     }
 
     public void leer_estadisticas() {
 
+        ranking.obtenirRanking();
         
     }
+
+    public void guardar_estadisticas() {
+
+      //  ranking.guardarRanking();
+    }
+
 
 
 
