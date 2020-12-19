08e81142c1dc47c736e1bfafdb5f51fc584e36d7
Victor Manuel Delgado Padilla <victor.manuel.delgado@estudiantat.upc.edu>
2020-12-19 18:26:12 +0100

    Actualización vista Perfil

--- FONTS/presentacion/VistaPerfil.java
+++ FONTS/presentacion/VistaPerfil.java
@@ -1,4 +1,19 @@
 package presentacion;
 
+import domain.usuari.Perfil;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+import java.util.ArrayList;
+
 public class VistaPerfil {
+
+    @FXML
+    public HBox perfilsRow;
+
+    public void initialize() {
+        ArrayList<Perfil> perfiles = new ArrayList<Perfil>();
+
+    }
 }
