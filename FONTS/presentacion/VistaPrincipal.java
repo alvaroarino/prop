c837a3070465efcb22ff7f642d19bc218e2bbc29
Alvaro Ariño Cabau <alvaro.arino@est.fib.upc.edu>
2020-12-19 17:13:32 +0100

    VistaPrincipal

--- FONTS/presentacion/VistaPrincipal.java
+++ FONTS/presentacion/VistaPrincipal.java
@@ -1,4 +1,19 @@
 package presentacion;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class VistaPrincipal {
+    @FXML
+    public Label errorMessage;
+    @FXML
+    public TextField usernameTextField;
+    @FXML
+    public TextField passTextField;
+    @FXML
+    public Button loginButton;
+    @FXML
+    public Button signupButton;
 }
