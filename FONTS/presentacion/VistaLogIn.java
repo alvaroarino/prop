053382f1a2e9eda66606a39d5670a7ddacdafe10
Victor Manuel Delgado Padilla <victor.manuel.delgado@estudiantat.upc.edu>
2020-12-20 16:57:08 +0100

    PassField arreglado

--- FONTS/presentacion/VistaLogIn.java
+++ FONTS/presentacion/VistaLogIn.java
@@ -8,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -19,7 +20,7 @@ public class VistaLogIn {
     @FXML
     public TextField usernameTextField;
     @FXML
-    public TextField passTextField;
+    public PasswordField passTextField;
     @FXML
     public Button loginButton;
     @FXML
