e7875f010b8a05560370711006b96703129c4f7b
Victor Manuel Delgado Padilla <victor.manuel.delgado@estudiantat.upc.edu>
2020-12-21 11:44:10 +0100

    Avance en vistas 2.0 ^^

---
+++ FONTS/presentacion/VistaAjustesPredefinidos.java
@@ -0,0 +1,22 @@
+package presentacion;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+public class VistaAjustesPredefinidos {
+    @FXML
+    public Button ButtonEmpezar;
+    @FXML
+    public Label nombreUsuarioLabel1;
+    @FXML
+    public TextField TextFieldDim;
+    @FXML
+    public TextField TextFieldBlancas;
+    @FXML
+    public TextField TextFieldNegras;
+    @FXML
+    public TextField TextFieldValor;
+}
---
+++ FONTS/presentacion/VistaPerfilPropio.java
@@ -0,0 +1,17 @@
+package presentacion;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+public class VistaPerfilPropio {
+    @FXML
+    public HBox perfilsRow;
+    @FXML
+    public Label LabelPerfil;
+    @FXML
+    public Label LabelUsuario;
+    @FXML
+    public Button ButtonSalir;
+}
